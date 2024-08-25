da5fe387d09c4b790f966679593ad86917b963d3
sandeep kumar <sandeepypb@gmail.com>
2024-08-26 00:11:56 +0530

    🎨 Style: update design

--- components/Shared/ProductDetails.tsx
+++ components/Shared/ProductDetails.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import Carousel from "./Carousel";
 import { useState } from "react";
 import { IoLocationOutline } from "react-icons/io5";
 import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const ProductDetails = () => {
   const images = ["/assets/p1.webp", "/assets/p2.webp", "/assets/p3.webp", "/assets/p4.webp", "/assets/p5.webp"];
@@ -26,26 +26,25 @@ const LeftGallaryView = ({ images }: { images: string[] }) => {
   };
 
   return (
-    <div className="gap-2 tablet:gap-5 flex justify-center py-8 h-fit">
-      <div className="w-[80px]">
-        <Carousel isVertical infinite nodots>
-          {images.map((image, index) => (
+    <div className="flex justify-center h-[330px] mobile:h-[420px]">
+      <div className="relative flex flex-col w-[80px] h-full">
+        {images.map((image, index) => (
+          <div key={index} className="relative cursor-pointer border m-[2px] h-[80px] bg-gray-100">
             <Image
+              layout="fill"
               key={index}
               src={image}
               alt="product"
-              width={20}
-              height={0}
-              sizes="100vw"
+              objectFit="contain"
+              objectPosition="center"
               onClick={() => handleSlideChange(index)}
-              className="cursor-pointer"
             />
-          ))}
-        </Carousel>
+          </div>
+        ))}
       </div>
 
-      <div className="">
-        <Image src={currentSlide} alt="product" width={400} height={0} sizes="100vw" />
+      <div className="relative w-[350px] border ">
+        <Image layout="fill" src={currentSlide} quality={100} objectFit="contain" objectPosition="center" alt="product" />
       </div>
     </div>
   );
@@ -54,74 +53,78 @@ const LeftGallaryView = ({ images }: { images: string[] }) => {
 const ProductDetail = () => {
   const sizes = ["S", "M", "L", "XL", "2XL"];
 
-  const keyHighlight = {
-    Design: "Graphic design",
-    Color: "Black",
-    "Wash Care": "Hand Wash",
-    Fabric: "100% Cotton",
-    Neck: "V-Neck",
-    Sleeve: "Half Sleeve",
-    Style: "Classic",
-  };
+  const keyHighlight = [
+    { key: "Design", value: "Graphic design" },
+    { key: "Color", value: "Black" },
+    { key: "Wash Care", value: "Hand Wash" },
+    { key: "Fabric", value: "100% Cotton" },
+    { key: "Neck", value: "V-Neck" },
+    { key: "Sleeve", value: "Half Sleeve" },
+    { key: "Style", value: "Classic" },
+  ];
 
   return (
-    <div className="flex-1 px-2 tablet:px-5 laptop:px-10">
-      <h2 className="text-3xl text-dark-3 font-bold mt-8">Product name </h2>
-      <p className="mt-5 text-lg">
+    <div className="w-full px-2 tablet:px-5 laptop:px-10 mt-5 tablet:mt-0">
+      <h2 className="head-text text-dark-3 font-bold">Product name </h2>
+      <p className="text-sm mobile:text-lg">
         Product description Sapiente iste placeat at qui voluptatem eaque dolore itaque numquam. Nihil omnis velit non facere
         sequi. Vel qui quasi nobis ipsa assumenda. Similique explicabo delectus.
       </p>
 
-      <p className="text-3xl font-semibold mt-5">Rs. 5000.00</p>
-      <p className="text-sm font-normal text-light-3">MRP incl. of all taxes</p>
+      <p className="head-text font-semibold mt-5">Rs. 5000.00</p>
+      <p className="text-sm font-normal text-light-3 line-through">MRP 5000.00</p>
 
       <div className="border w-fit border-light-3 px-3 my-5">
-        <p className="text-lg font-semibold text-light-3">100% cotton</p>
+        <p className="text-base font-semibold text-light-3">100% cotton</p>
       </div>
 
-      <p className="text-lg font-semibold text-dark-3 uppercase mt-10">Select Size</p>
-      <div className="flex gap-5 mt-3">
+      <p className="text-base mobile:text-lg font-semibold text-dark-3 uppercase mt-5">Select Size</p>
+      <div className="flex gap-1">
         {sizes.map((size) => (
           <div key={size} className="border border-light-3 px-3 py-1 cursor-pointer">
-            <p className="text-lg font-semibold text-dark-3">{size}</p>
+            <p className="text-base mobile:text-lg font-semibold text-dark-3">{size}</p>
           </div>
         ))}
       </div>
 
-      <div className="flex gap-5 py-5 tablet:py-10">
-        <button className="w-full bg-success text-white rounded-[5px] uppercase text-md font-bold h-[50px] mt-5">
+      <div className="flex gap-2 py-5">
+        <button className=" bg-success text-white rounded-[5px] uppercase text-sm tablet:text-base font-bold p-2 px-4">
           Add to cart
         </button>
-        <button className="w-full bg-secondary text-white rounded-[5px] uppercase text-md font-bold h-[50px] mt-5">
+        <button className=" bg-secondary text-white rounded-[5px] uppercase text-sm tablet:text-base font-bold p-2 px-4">
           Add to wishlist
         </button>
       </div>
 
       <div>
         <span className="flex items-center gap-2">
-          <IoLocationOutline className="text-2xl" />
-          <p className="text-lg font-semibold text-dark-3 uppercase">Check for delivery details</p>
+          <IoLocationOutline className="text-base" />
+          <p className="text-base font-semibold text-dark-3">Check for delivery details</p>
         </span>
-        <p className="text-lg font-normal text-light-3">Delivering all over India</p>
+        <p className="text-sm mobile:text-base font-normal text-light-3">Delivering all over India</p>
 
-        <div className="border my-5 py-1 flex items-center gap-5">
+        <div className="border my-3 flex items-center gap-5 w-full tablet:w-[300px]">
           <Input
             type="text"
             placeholder="Enter Pincode"
-            className="border-0 focus-visible:ring-transparent shadow-none text-lg"
+            className="border-0 focus-visible:ring-transparent shadow-none text-base "
           />
-          <p className="text-base font-semibold text-success uppercase mr-3 cursor-pointer">Check</p>
+          <Button variant="outline" className="border-none uppercase ">
+            Check
+          </Button>
         </div>
       </div>
 
-      <p className="text-2xl font-semibold text-dark-3 mt-10">Key Highlights</p>
-      <ul className="list-disc list-inside">
-        {Object.entries(keyHighlight).map(([key, value]) => (
-          <li key={key} className="text-lg text-dark-3">
-            {key}: {value}
-          </li>
+      <p className="text-xl font-semibold text-dark-3 mt-10">Key Highlights</p>
+      <div className="grid grid-cols-2 gap-5 mt-3">
+        {keyHighlight.map((item, index) => (
+          <div key={index} className="text-lg text-dark-3">
+            <p className="font-semibold">{item.key}</p>
+            <p>{item.value}</p>
+            <div className="w-full mobile:w-1/2 h-[1px] bg-light-3"></div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
