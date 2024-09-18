29438f3ac35c367b84bfcd5177271d0a78fedc58
sandeep kumar <sandeepypb@gmail.com>
2024-09-19 00:38:04 +0530

    🔨 Refactor: add active nav link

--- components/Shared/BottomBar.tsx
+++ components/Shared/BottomBar.tsx
@@ -3,7 +3,7 @@
 import { useData } from "@/hooks/useData";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import React from "react";
+import React, { useMemo } from "react";
 import { IoCartOutline, IoHomeOutline, IoPersonOutline, IoSearchOutline } from "react-icons/io5";
 import { PiPackageLight } from "react-icons/pi";
 
@@ -17,16 +17,16 @@ const BottomBar = () => {
     <div className="fixed bottom-0 left-0 right-0 tablet:hidden border-t border-border z-10">
       <div className="bg-background h-12 mobile:h-14 w-full">
         <div className="flex justify-around items-center h-full">
-          <Link href="/">
+          <NavLink href="/">
             <IoHomeOutline className="text-2xl" />
-          </Link>
-          <Link href="/myaccount/orders">
+          </NavLink>
+          <NavLink href="/myaccount/orders">
             <PiPackageLight className="text-2xl" />
-          </Link>
-          <Link href="/search">
+          </NavLink>
+          <NavLink href="/search">
             <IoSearchOutline className="text-2xl" />
-          </Link>
-          <Link href="/cart">
+          </NavLink>
+          <NavLink href="/cart">
             <span className="relative" onClick={() => router.push("/cart")}>
               {cart?.length > 0 && (
                 <span className="absolute -top-1 -right-1 text-xs font-semibold text-white bg-green-600 rounded-full w-4 h-4 flex justify-center items-center">
@@ -35,13 +35,26 @@ const BottomBar = () => {
               )}
               <IoCartOutline className="text-2xl" />
             </span>
-          </Link>
-          <Link href="/myaccount">
+          </NavLink>
+          <NavLink href="/myaccount">
             <IoPersonOutline className="text-2xl" />
-          </Link>
+          </NavLink>
         </div>
       </div>
     </div>
+  );
+};
+
+const NavLink = ({ href, children }: { href: string; children: React.ReactNode }) => {
+  const pathname = usePathname();
+  return (
+    <Link
+      href={href}
+      className={`border-b-4 border-transparent hover:border-primary`}
+      style={{ borderColor: pathname === href ? "var(--primary)" : "" }}
+    >
+      {children}
+    </Link>
   );
 };
 
