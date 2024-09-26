4cf5432f55f44c99d418b470871bb1bf1bbde3c4
sandeep kumar <sandeepypb@gmail.com>
2024-09-26 19:23:33 +0530

    📚 Docs: add build folder
    Former-commit-id: b74e2c607fdde9e90de68cacfcea463a122d738b

---
+++ build/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"KQ3oRs6ELXQRxt3fKg3BT6sDJ9n6LalI6OqwKdiibao=\"}"
--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  distDir: "build",
   images: {
     remotePatterns: [
       {
